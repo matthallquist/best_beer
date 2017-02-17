65ff95b26dbea0ade9ce5bac6721d28cee5ea10a
Matthew Hallquist <matthallquist@yahoo.com>
2017-02-17 17:44:13 +0000

    added options method for CLI to clean up looping back through menus

--- lib/best_beer.rb
+++ lib/best_beer.rb
@@ -8,6 +8,22 @@ require_relative "beer.rb"
 require_relative "brewery.rb"
 require_relative "beer_scraper.rb"
 
+  def the_options
+    puts "Enter a beer's rating, menu, or exit."
+    input = gets.strip
+    if input == "menu"
+      menu
+    elsif input == "exit"
+      say_goodbye
+    elsif input.to_i.between?(1,250)
+      more_beer_info(input)
+      the_options
+    else
+      puts "I'm not quite sure what you mean."
+      the_options
+    end
+  end
+
   def start
     BeerScraper.create_beers
     menu
@@ -18,93 +34,17 @@ require_relative "beer_scraper.rb"
     input = gets.strip
     if input == "1"
       top_beers
-      input = gets.strip
-      if input == "menu"
-        menu
-      elsif input == "exit"
-        say_goodbye
-      elsif input.to_i.between?(1,250)
-        more_beer_info(input)
-        input = gets.strip
-        if input == "menu"
-          menu
-        elsif input == "exit"
-          say_goodbye
-        else
-          puts "Sorry, didn't catch that!"
-          menu
-        end
-      else
-        puts "Sorry, didn't catch that!"
-        menu
-      end
+      the_options
     elsif input == "2"
       top_25_beers
-      input = gets.strip
-      if input == "menu"
-        menu
-      elsif input == "exit"
-        say_goodbye
-      elsif input.to_i.between?(1,250)
-        more_beer_info(input)
-        input = gets.strip
-        if input == "menu"
-          menu
-        elsif input == "exit"
-          say_goodbye
-        else
-          puts "Sorry, didn't catch that!"
-          menu
-        end
-      else
-        puts "Sorry, didn't catch that!"
-        menu
-      end
+      the_options
     elsif input == "3"
       top_50_beers
-      input = gets.strip
-      if input == "menu"
-        menu
-      elsif input == "exit"
-        say_goodbye
-      elsif input.to_i.between?(1,250)
-        more_beer_info(input)
-        input = gets.strip
-        if input == "menu"
-          menu
-        elsif input == "exit"
-          say_goodbye
-        else
-          puts "Sorry, didn't catch that!"
-          menu
-        end
-      else
-        puts "Sorry, didn't catch that!"
-        menu
-      end
+      the_options
     elsif input == "4"
       top_100_beers
-      input = gets.strip
-      if input == "menu"
-        menu
-      elsif input == "exit"
-        say_goodbye
-      elsif input.to_i.between?(1,250)
-        more_beer_info(input)
-        input = gets.strip
-        if input == "menu"
-          menu
-        elsif input == "exit"
-          say_goodbye
-        else
-          puts "Sorry, didn't catch that!"
-          menu
-        end
-      else
-        puts "Sorry, didn't catch that!"
-        menu
-      end
-    elsif input == "5"
+      the_options
+    elsif input == "exit"
       say_goodbye
     else
       puts "Sorry, didn't catch that!"
@@ -116,12 +56,11 @@ require_relative "beer_scraper.rb"
   def show_menu
     puts "What would you like to view?"
     puts "MENU:"
-    puts "Enter the number next to your choice"
     puts "1 - Top Beers"
     puts "2 - Top 25 Beers"
     puts "3 - Top 50 Beers"
     puts "4 - Top 100 Beers"
-    puts "5 - Exit"
+    puts "or exit"
   end
 
   def say_goodbye
@@ -133,8 +72,6 @@ require_relative "beer_scraper.rb"
     Beer.all.each do |b|
       puts "#{b.rating}. #{b.name} - #{b.brewery.name}"
     end
-    puts "Enter a beer's rating to see more info."
-    puts "Options: 'menu', 'exit'"
   end
 
   def top_25_beers
@@ -142,8 +79,6 @@ require_relative "beer_scraper.rb"
     Beer.all[0,25].each do |b|
       puts "#{b.rating}. #{b.name} - #{b.brewery.name}"
     end
-    puts "Enter a beer's rating to see more info."
-    puts "Options: 'menu', 'exit'"
   end
 
   def top_50_beers
@@ -151,8 +86,6 @@ require_relative "beer_scraper.rb"
     Beer.all[0,50].each do |b|
       puts "#{b.rating}. #{b.name} - #{b.brewery.name}"
     end
-    puts "Enter a beer's rating to see more info."
-    puts "Options: 'menu', 'exit'"
   end
 
   def top_100_beers
@@ -160,13 +93,10 @@ require_relative "beer_scraper.rb"
     Beer.all[0,100].each do |b|
       puts "#{b.rating}. #{b.name} - #{b.brewery.name}"
     end
-    puts "Enter a beer's rating to see more info."
-    puts "Options: 'menu', 'exit'"
   end
 
   def more_beer_info(beer_rating)
     beer = Beer.all.find {|b| b.rating == beer_rating}
     puts "Further information on #{beer.name} by #{beer.brewery.name}:"
     puts "#{beer.rating}. #{beer.name} - #{beer.brewery.name} - #{beer.type} - #{beer.abv}"
-    puts "Options: 'menu', 'exit'"
   end
